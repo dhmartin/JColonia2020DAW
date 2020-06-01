bae41fe5fa56eb8a54a09df405044f1cc502dcf5
David H. Martín <dmartin.jcolonia@gmail.com>
2020-06-01 17:45:44 +0200

    Instalación Windows para WindowBuilder

---
+++ eclipse2020-03/w10_install64-windowbuilder.cmd
@@ -0,0 +1,27 @@
+@CHCP 65001 > NUL
+@ECHO OFF
+
+ECHO Instalación de WindowBuilder para ECLIPSE
+ECHO =========================================
+ECHO.
+
+SET SRCDIR=%~dp0
+SET WBDEST=%ProgramFiles%\eclipse\dropins
+
+IF EXIST "%WBDEST%" (
+	IF EXIST "%WBDEST%\WindowBuilder" RMDIR /S /Q "%WBDEST%\WindowBuilder"
+	
+	FOR %%F IN ("%SRCDIR%WindowBuilder\repository.zip") DO (
+		ECHO Descomprimiendo %%~nxF…
+		MKDIR "%WBDEST%\%%~nF"
+		MKDIR "%WBDEST%\%%~nF\eclipse"
+		"%SRCDIR%unzip" -qnd "%WBDEST%\%%~nF\eclipse" "%%~fF"
+		DEL /Q "%WBDEST%\%%~nF\eclipse\*.*"
+		MOVE "%WBDEST%\%%~nF" "%WBDEST%\WindowBuilder"
+	)
+)
+
+SET SRCDIR=
+SET WBDEST=
+
+PAUSE
