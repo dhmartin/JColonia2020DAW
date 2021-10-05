d49e08a9e5bee22e1dcf88a555ef04bd7f2fd6be
David H. Martín <dmartin.jcolonia@gmail.com>
2021-10-05 16:35:45 +0200

    Incorporadas traducciones sobre servidores de lenguajes LSP4E

--- Scripts eclipse/Babel/descargar.sh
+++ Scripts eclipse/Babel/descargar.sh
@@ -16,7 +16,7 @@ FECHA=$(lynx -dump "${URL0}" |
 lynx -dump "${URL0}/${FECHA}/${VERSION_ECLIPSE}/${VERSION_ECLIPSE}.php" |
 	grep -E 'https://.+BabelLanguagePack.+-(es|en_AA)_[^\s]+\.zip$' |
 	sed -E 's/.+ (https:\/\/[^/s]+)/\1/' |
-	grep -E '(cdt-es|datatools-es|eclipse-es|git-es|mpc-es|mylyn-es|rap-es|webtools-es|en_AA)_' |
+	grep -E '(cdt-es|datatools-es|eclipse-es|git-es|lsp4e-es|mpc-es|mylyn-es|rap-es|webtools-es|en_AA)_' |
 	xargs wget -c
 
 ls -la BabelLanguagePack*.zip | wc -l
