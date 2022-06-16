c1c94bcfb446baf4d1ae428461df153ff82d2858
David H. Martín <dmartin.jcolonia@gmail.com>
2022-06-16 19:38:56 +0200

    Actualización a eclipse 2022-06

--- Scripts eclipse/w10_install64.cmd
+++ Scripts eclipse/w10_install64.cmd
@@ -1,7 +1,7 @@
 @CHCP 65001 > NUL
 @ECHO OFF
 
-ECHO Instalación de ECLIPSE 2022-03
+ECHO Instalación de ECLIPSE 2022-06
 ECHO ==============================
 ECHO.
 
@@ -13,7 +13,7 @@ SET PROGRAMDEST=
 IF NOT DEFINED PROGRAMDEST SET PROGRAMDEST=%ProgramFiles%
 
 ECHO Descomprimiendo «eclipse» en %PROGRAMDEST%:
-"%SRCDIR%\unzip" -qnd "%PROGRAMDEST%" "%SRCDIR%\eclipse-java-2022-03-R-win32-x86_64.zip"
+"%SRCDIR%\unzip" -qnd "%PROGRAMDEST%" "%SRCDIR%\eclipse-java-2022-06-R-win32-x86_64.zip"
 ECHO.
 
 ECHO Descomprimiendo «openJDK11» en %JAVADEST%:
@@ -28,7 +28,7 @@ ECHO.
 
 ECHO Descomprimiendo «doxygen» y «graphviz» en %JAVADEST%:
 MKDIR "%JAVADEST%\doxygen"
-"%SRCDIR%\unzip" -qnd "%JAVADEST%\doxygen" "%SRCDIR%\doxygen-1.9.3.windows.x64.bin.zip"
+"%SRCDIR%\unzip" -qnd "%JAVADEST%\doxygen" "%SRCDIR%\doxygen-1.9.4.windows.x64.bin.zip"
 "%SRCDIR%\unzip" -qnd "%JAVADEST%" "%SRCDIR%\windows_10_msbuild_Release_graphviz-3.0.0-win32.zip"
 
 MOVE "%JAVADEST%\Graphviz" "%JAVADEST%\graphviz.0"
