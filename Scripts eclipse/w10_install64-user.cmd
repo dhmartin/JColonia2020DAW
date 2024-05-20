6b59260390de89182174926223d0085df3de5621
David H. Martín <dmartin.jcolonia@gmail.com>
2024-05-20 19:27:52 +0200

    Incorporación variable 7za en script de usuario W10

--- Scripts eclipse/w10_install64-user.cmd
+++ Scripts eclipse/w10_install64-user.cmd
@@ -10,26 +10,30 @@ SET ICONDEST=%USERPROFILE%\Desktop
 SET JAVADEST=C:\java
 SET PROGRAMDEST=C:\java
 
+REM SET UNZIP_DEST="%SRCDIR%\unzip" -nd
+SET UNZIP_DEST="%SRCDIR%\7za" x -bso0 -bd -o
+
+
 IF NOT DEFINED PROGRAMDEST SET PROGRAMDEST=%ProgramFiles%
 
 ECHO Descomprimiendo «eclipse» en %PROGRAMDEST%:
-"%SRCDIR%\7za" x -bso0 -bd -o"%PROGRAMDEST%" "%SRCDIR%\eclipse-java-2024-03-R-win32-x86_64.zip"
+%UNZIP_DEST%"%PROGRAMDEST%" "%SRCDIR%\eclipse-java-2024-03-R-win32-x86_64.zip"
 ECHO.
 
 ECHO Descomprimiendo «openJDK17» en %JAVADEST%:
-"%SRCDIR%\7za" x -bso0 -bd -o"%JAVADEST%" "%SRCDIR%\OpenJDK17U-jdk_x64_windows_hotspot_17.0.11_9.zip"
+%UNZIP_DEST%"%JAVADEST%" "%SRCDIR%\OpenJDK17U-jdk_x64_windows_hotspot_17.0.11_9.zip"
 MOVE "%JAVADEST%\jdk-17*" "%JAVADEST%\jdk17"
 ECHO.
 
 ECHO Descomprimiendo «openJDK21» en %JAVADEST%:
-"%SRCDIR%\7za" x -bso0 -bd -o"%JAVADEST%" "%SRCDIR%\OpenJDK21U-jdk_x64_windows_hotspot_21.0.3_9.zip"
+%UNZIP_DEST%"%JAVADEST%" "%SRCDIR%\OpenJDK21U-jdk_x64_windows_hotspot_21.0.3_9.zip"
 MOVE "%JAVADEST%\jdk-21*" "%JAVADEST%\jdk21"
 ECHO.
 
 ECHO Descomprimiendo «doxygen» y «graphviz» en %JAVADEST%:
 MKDIR "%JAVADEST%\doxygen"
-"%SRCDIR%\7za" x -bso0 -bd -o"%JAVADEST%\doxygen" "%SRCDIR%\doxygen-1.10.0.windows.x64.bin.zip"
-"%SRCDIR%\7za" x -bso0 -bd -o"%JAVADEST%" "%SRCDIR%\windows_10_cmake_Release_Graphviz-11.0.0-win64.zip"
+%UNZIP_DEST%"%JAVADEST%\doxygen" "%SRCDIR%\doxygen-1.10.0.windows.x64.bin.zip"
+%UNZIP_DEST%"%JAVADEST%" "%SRCDIR%\windows_10_cmake_Release_Graphviz-11.0.0-win64.zip"
 
 MOVE "%JAVADEST%\Graphviz*" "%JAVADEST%\graphviz.0"
 MOVE "%JAVADEST%\graphviz.0\bin" "%JAVADEST%\graphviz"
