fd2e430f9caeb4c7686600912e1f5f452239d79e
David H. Martín <dmartin.jcolonia@gmail.com>
2025-09-02 20:25:43 +0200

    Mejoras para cambio de versiones

--- Scripts eclipse/descargaeclipse.sh
+++ Scripts eclipse/descargaeclipse.sh
@@ -6,17 +6,37 @@
 
 PROG=$(basename $0)
 
-ECLIPSE_VERSION=2025-06
-REPO=https://eclipse.mirror.garr.it/technology/epp/downloads/release/${ECLIPSE_VERSION}/R/
+ECLIPSE_VERSION=2025-09
+ECLIPSE_RELEASE=RC1
+REPO=https://eclipse.mirror.garr.it/technology/epp/downloads/release/${ECLIPSE_VERSION}/${ECLIPSE_RELEASE}/
 
 ARCHIVOS="
-    eclipse-java-${ECLIPSE_VERSION}-R-linux-gtk-x86_64.tar.gz
-    eclipse-java-${ECLIPSE_VERSION}-R-win32-x86_64.zip
-    eclipse-jee-${ECLIPSE_VERSION}-R-linux-gtk-x86_64.tar.gz
-    eclipse-jee-${ECLIPSE_VERSION}-R-win32-x86_64.zip
+	eclipse-java-${ECLIPSE_VERSION}-${ECLIPSE_RELEASE}-linux-gtk-x86_64.tar.gz
+	eclipse-java-${ECLIPSE_VERSION}-${ECLIPSE_RELEASE}-win32-x86_64.zip
+	eclipse-jee-${ECLIPSE_VERSION}-${ECLIPSE_RELEASE}-linux-gtk-x86_64.tar.gz
+	eclipse-jee-${ECLIPSE_VERSION}-${ECLIPSE_RELEASE}-win32-x86_64.zip
 "
 
 for file in ${ARCHIVOS}; do
     echo "$REPO$file"
     echo "$REPO$file.sha1"
 done | xargs wget -c
+
+
+ARCHIVOS="
+	limpiapdt.sh
+	linux_install64.sh
+	w10_install64.cmd
+	w10_install64-jee.cmd
+	w10_install64-user.cmd
+	w10_install64-user-jee.cmd
+"
+
+for file in ${ARCHIVOS}; do
+    echo "$file"
+done | xargs sed -i \
+	-e "s/[[:digit:]]\{4\}-[[:digit:]]\{2\}\(-\|\/\)\(R\|RC1\|RC2\|M1\|M2\)/${ECLIPSE_VERSION}\1${ECLIPSE_RELEASE}/g" \
+	-e "s/[[:digit:]]\{4\}-[[:digit:]]\{2\}\([^-]\)/${ECLIPSE_VERSION}\1/g"
+
+mkdirhier PDT
+ln -f limpiapdt.sh PDT
